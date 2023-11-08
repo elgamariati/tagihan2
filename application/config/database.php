318fe8d6d4a49f7b869d37f0b18c2922fc2b97ac
elgamariati <elgamariati@gmail.com>
2023-11-08 22:12:55 +0700

    delete data

--- application/config/database.php
+++ application/config/database.php
@@ -137,17 +137,3 @@ $db['simari'] = array(
 	'save_queries' => TRUE
 );
 // use Illuminate\Database\Capsule\Manager as Capsule;
-
-// $capsule = new Capsule;
-// $capsule->addConnection(array(
-// 	'driver'    => 'mysql',
-// 	'host'      => $db['default']['hostname'],
-// 	'database'  => $db['default']['database'],
-// 	'username'  => $db['default']['username'],
-// 	'password'  => $db['default']['password'],
-// 	'charset'   => 'utf8',
-// 	'collation' => 'utf8_unicode_ci',
-// 	'prefix'    => $db['default']['dbprefix'],
-// ));
-// $capsule->setAsGlobal();
-// $capsule->bootEloquent();
