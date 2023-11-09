ca4ee348b7b589f5bea3ebffda5d3e4a6a247fe0
elgamariati <elgamariati@gmail.com>
2023-11-09 11:34:10 +0700

    update config for local

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'https://tagihan.upr.ac.id';
+$config['base_url'] = 'http://localhost/tagihan/';
 
 /*
 |--------------------------------------------------------------------------
