7bd87e5e7ccee3109542e67ecdb3c938b94dcdb1
elgamariati <elgamariati@gmail.com>
2023-11-13 11:36:12 +0700

    add controller and view cicilan

---
+++ application/modules/cicilan/controllers/Cicilan.php
@@ -0,0 +1,31 @@
+<?php
+
+class Cicilan extends MY_Controller
+{
+	private $modul = 'cicilan';
+	private $semester_ref = array(
+		1 => "Ganjil",
+		2 => "Genap",
+		3 => "Antara"
+	);
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url');
+		$this->load->helper('form');
+		$this->load->helper('datetime_helper');
+		$this->load->helper('rupiah_helper');
+		$this->load->model('Laporan_m', 'database');
+		$this->load->model('periode/Periode_m', 'periode');
+		$this->load->model('simari/Simari_m', 'simari');
+		$this->load->model('atur_jenjang/Atur_jenjang_m', 'atur_jenjang');
+	}
+
+	public function index()
+	{
+        $aksi_modul = 'baca';
+		if (!$this->acl->cek_akses_module($this->role, $this->modul, $aksi_modul))
+			show_error($this->acl->body_text, 401, $this->acl->header_text);
+		$this->layout->render('index');
+	}
+}
---
+++ application/modules/cicilan/views/cicil_v.php
@@ -0,0 +1,127 @@
+ <!-- BEGIN PAGE CONTAINER-->
+ <div class="page-content">
+     <!-- BEGIN SAMPLE PORTLET CONFIGURATION MODAL FORM-->
+     <div class="content">
+         <div class="page-title col-lg-8 col-md-8 col-sm-8 col-xs-8">
+             <i class="fa fa fa-server"></i>
+             <h3><span class="semi-bold">Useraaaaaaaaaaaaaaaaaa</span><span style="font-size : 11pt ;"> Manajemen user dan password aplikasi.</span></h3>
+         </div>
+         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+             <a href="#" data-toggle="modal" data-backdrop="static" data-target="#modal-form" class="btn btn-primary btn-cons" style="float : right ; margin-right : -14px"><i class="fa fa-plus ico-space"></i></i><span>Tambah Data</span></a>
+         </div>
+
+         <!--       <div class="col-lg-4 col-md-4">
+          <a href="#" data-toggle="modal" data-backdrop="static" data-target="#modal-form" class="btn btn-primary btn-cons" style="float : right ; margin-right : -14px"><i class="fa fa-plus ico-space"></i></i><span>Tambah Data</span></a>
+       </div>-->
+         <div class="row-fluid">
+             <div class="span12">
+                 <div class="grid simple ">
+                     <div class="grid-title">
+                         <h4>Tabel <span class="semi-boldd">User</span></h4>
+                         <div class="tools">
+                             <a href="javascript:;" class="collapse"></a>
+                         </div>
+
+                     </div>
+                     <div class="grid-body ">
+                         <div class="row">
+                             <div class="col-md-4 pull-right">
+                                 <div class="input-group">
+                                     <input type="text" id="field-cari" class="form-control" name="field-cari">
+                                     <span class="input-group-btn">
+                                         <!--<input type="button" id="btn-cari" class="btn btn-default" value="Cari" />-->
+                                         <a class="btn btn-primary" id="btn-cari" href="#" value="Cari"><i class="fa fa-search"></i></a>
+                                     </span>
+                                 </div>
+                             </div>
+                         </div>
+                         <table class="table table-hover table-condensed" id="table-user">
+                             <thead>
+                                 <tr>
+                                     <th>No</th>
+                                     <th>Username</th>
+                                     <th>Nama</th>
+                                     <th>Role</th>
+                                     <th width="auto%">Opsi</th>
+                                 </tr>
+                             </thead>
+                         </table>
+                     </div>
+                 </div>
+             </div>
+
+         </div>
+
+     </div>
+
+
+
+ </div>
+ <!--<div class="col-md-4 pull-right">
+      <div class="input-group" >
+          <input  type="text" id="field-cari" class="form-control" name="field-cari" placeholderr="Pencarian">
+          <span class="input-group-btn" >
+              <a class="btn btn-primary" id="btn-cari" href="#" value="Cari"><i class="fa fa-search"></i></a>
+          </span>
+      </div>
+  </div>-->
+
+ <!--Modal Hapus-->
+ <div id="modal-hapus" class="modal fade" tabindex="-1" role="dialog">
+     <div class="modal-dialog modal-sm">
+         <div class="modal-content">
+             <div class="modal-header"><span class="semi-boldd">Hapus User</span></div>
+             <div class="modal-body">
+                 Anda yakin menghapus User dengan Username "<span id="id-delete"></span>"?
+             </div>
+             <div class="modal-footer2" style="text-align: center;padding-bottom: 20px;">
+                 <button id="btn-hapus" class="btn btn-danger" onclick="hapus();">
+                     <span class="fa fa-spinner fa-spin"></span> Hapus
+                 </button>
+                 <button id="btn-batal" data-dismiss="modal" class="btn btn-primary">Tutup</button>
+             </div>
+         </div>
+     </div>
+ </div>
+
+ <!--Modal Form -->
+ <div id="modal-form" class="modal fade" tabindex="-1" role="dialog">
+     <div class="modal-dialog">
+         <div class="modal-content">
+             <div class="modal-header">
+                 <h5 id="modal-title">Tambah User</h5>
+             </div>
+             <div class="modal-body">
+                 <div class="alert alert-danger" id="pesan-error"></div>
+                 <?php $this->view('_form', array('listRole' => $listRole)); ?>
+             </div>
+             <div class="modal-footer">
+                 <button id="btn-simpan" class="btn btn-primary modalbtn" onclick="simpan();">
+                     <span class="fa fa-spinner fa-spin"></span> <span class="fa fa-floppy-o"></span> Simpan
+                 </button>
+                 <button id="btn-batal-simpan" data-dismiss="modal" class="btn modalbtn">Tutup</button>
+             </div>
+         </div>
+     </div>
+ </div>
+
+ <!--Modal Reset-->
+ <div id="modal-reset" class="modal fade" tabindex="-1" role="dialog">
+     <div class="modal-dialog modal-sm">
+         <div class="modal-content">
+             <div class="modal-header">Reset Password</div>
+             <div class="modal-body">
+                 Anda akan mereset password dengan username
+                 "<span id="user-reset"></span>" ?
+             </div>
+             <div class="modal-footer2" style="text-align: center">
+                 <button id="btn-reset" class="btn btn-success" onclick="reset();">
+                     <span class="fa fa-spinner fa-spin"></span> Reset
+                 </button>
+                 <button id="btn-batalR" data-dismiss="modal" class="btn btn-default">Tutup</button>
+             </div>
+         </div>
+     </div>
+ </div>
+
+
